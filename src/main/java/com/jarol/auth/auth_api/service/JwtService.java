a0d153e900f46b6e0dc606fa18976a0384864000
JarolTellez <jarol.tellez228188@potros.itson.edu.mx>
2026-04-08 12:12:10 -0700

    refactor: user Id as a token subject

--- src/main/java/com/jarol/auth/auth_api/service/JwtService.java
+++ src/main/java/com/jarol/auth/auth_api/service/JwtService.java
@@ -1,66 +1,69 @@
 package com.jarol.auth.auth_api.service;
 
+import com.jarol.auth.auth_api.config.JwtProperties;
 import com.jarol.auth.auth_api.model.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class JwtService {
 
-    @Value("${jwt.secret}")
-    private String secret;
+    private final JwtProperties jwtProperties;
+    private SecretKey key;
 
-    @Value("${jwt.access-expiration}")
-    private long accessExpiration;
-
-    @Value("${jwt.refresh-expiration}")
-    private long refreshExpiration;
-
-
-    public String generateAccessToken(User user){
-        return buildToken(user, accessExpiration);
+    @PostConstruct
+    public void init() {
+        byte[] keyBytes = Decoders.BASE64.decode(jwtProperties.getSecret());
+        key = Keys.hmacShaKeyFor(keyBytes);
     }
 
-    public String generateRefreshToken(User user){
-        return buildToken(user, refreshExpiration);
+
+    public String generateAccessToken(User user, UUID sessionId) {
+        return buildToken(user, sessionId, jwtProperties.getAccessExpiration());
     }
 
-    private String buildToken(User user, long expiration){
+    public String generateRefreshToken(User user, UUID sessionId) {
+        return buildToken(user, sessionId, jwtProperties.getRefreshExpiration());
+    }
+
+    private String buildToken(User user, UUID sessionId, long expiration) {
         return Jwts.builder()
-                .subject(user.getEmail())
+                .subject(user.getId().toString())
+                .claim("sessionId", sessionId.toString())
                 .issuedAt(new Date())
-                .expiration(new Date(System.currentTimeMillis()+expiration))
+                .expiration(new Date(System.currentTimeMillis() + expiration))
                 .signWith(getSignKey())
                 .compact();
     }
 
-    private SecretKey getSignKey(){
-        byte[] keyBytes = Decoders.BASE64.decode(secret);
-        return Keys.hmacShaKeyFor(keyBytes);
+    private SecretKey getSignKey() {
+        return key;
     }
 
-    public String extractUsername(String token){
+    public String extractUsername(String token) {
         return extractClaims(token).getSubject();
     }
-    public boolean isTokenValid(String token, User user){
-        final String username= extractUsername(token);
-        return username.equals(user.getEmail())&&!isTokenExpired(token);
+
+    public boolean isTokenValid(String token, User user) {
+        final String username = extractUsername(token);
+        return username.equals(user.getEmail()) && !isTokenExpired(token);
     }
 
-    public boolean isTokenExpired(String token){
+    public boolean isTokenExpired(String token) {
         return extractClaims(token).getExpiration().before(new Date());
     }
 
-    private Claims extractClaims(String token){
+    private Claims extractClaims(String token) {
         return Jwts.parser()
                 .verifyWith(getSignKey())
                 .build()
